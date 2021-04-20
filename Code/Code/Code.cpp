c924dda3c2564021dce6a80ac6f3696273e04e0a
Skunkmeister <c1a55yc0d3r@gmail.com>
2021-04-19 23:39:56 -0400

    Adjusted naming of old version of neural network which wasn't quite ready and was an almost direct copy of the tutorial with abundant commenting

--- Code/Code/Code.cpp
+++ Code/Code/Code.cpp
@@ -23,6 +23,7 @@ TrainingData::TrainingData(const std::string filename)
     std::string temp;
     std::getline(m_trainingDataFile, temp);
 }
+
 std::pair<unsigned int, unsigned int> TrainingData::getData(std::vector<double> &in, std::vector<double> &out)
 {
     in.clear();
@@ -83,8 +84,8 @@ class Neuron
         double sumDOW(const Layer &nextLayer) const;
 };
 
-double Neuron::eta = 0.35;
-double Neuron::alpha = 0.5;
+double Neuron::eta = 0.05;
+double Neuron::alpha = 0.05;
 
 Neuron::Neuron(unsigned int numOutputs, unsigned int myIndex)
 {
@@ -363,12 +364,14 @@ void train()
     topology.push_back(4);
     topology.push_back(5);
     topology.push_back(5);
+	topology.push_back(5);
+	topology.push_back(5);
     topology.push_back(3);
     Net myNet(topology);
-    std::ifstream weights;
+    /**std::ifstream weights;
     weights.open("weights.csv");
     if(weights)
-        myNet.loadWeights();
+        myNet.loadWeights(); */
 
     std::vector<double> inputVals, targetVals, resultVals;
     int trainingPass = 0;
@@ -400,11 +403,86 @@ void train()
         std::cout<<std::endl<<"Done!"<<std::endl;
 }
 
+void loadMatrices(Eigen::MatrixXd &A, Eigen::MatrixXd &out1, Eigen::MatrixXd &out2, Eigen::MatrixXd &out3)
+{
+	std::ifstream in;
+	in.open("data.csv");
+	std::string data;
+	std::getline(in, data, '\n'); //ditch the first line
+	for (int i = 0; i < 642426; i++) //for each layer except last one
+	{
+		std::getline(in, data, ','); //ditch date double
+		std::getline(in, data, ','); //sex
+		A(i, 0) = stod(data);
+
+		std::getline(in, data, ','); //age
+		A(i, 1) = stod(data);
+
+		std::getline(in, data, ','); //race
+		A(i, 2) = stod(data);
+
+		std::getline(in, data, ','); //med
+		A(i, 3) = stod(data);
+
+		std::getline(in, data, ','); //isHospitalized?
+		out1(i, 0) = stod(data);
+
+		std::getline(in, data, ','); //isICU?
+		out2(i, 0) = stod(data);
+
+		std::getline(in, data, ','); //isDead?
+		out3(i, 0) = stod(data);
+
+	}
+}
+
+Eigen::MatrixXd s1(3, 1);
+Eigen::MatrixXd s2(3, 1);
+Eigen::MatrixXd s3(3, 1);
+double error1, error2, error3;
+
 void linReg()
 {
-
+	//Linear solve a with respect to b1 (multiple least squares regression for all inputs with respect to isHospitalized)
+	std::cout << "Least Squares Multiple Regression with Respect to isHospitalized: " << std::endl;
+	std::cout << "Least Squares Solution: " << s1 << std::endl;
+	std::cout << "MSE: " << error1 << std::endl;
+	std::cout << std::endl;
+	
+	//Linear solve a with respect to b2 (multiple least squares regression for all inputs with respect to isICU)
+	std::cout << "Least Squares Multiple Regression with Respect to isICU: " << std::endl;
+	std::cout << "Least Squares Solution: " << s2 << std::endl;
+	std::cout << "MSE: " << error2 << std::endl;
+	std::cout << std::endl;
+	
+	//Linear solve a with respect to b3 (multiple least squares regression for all inputs with respect to isDead?)
+	std::cout << "Least Squares Multiple Regression with Respect to isDead?: " << std::endl;
+	std::cout << "Least Squares Solution: " << s3 << std::endl;
+	std::cout << "MSE: " << error3 << std::endl;
+	std::cout << std::endl;
 
 }
+
+std::vector<double> calcLinearReg(double sex, double age, double race, double medCond)
+{
+	std::vector<double> solution;
+	solution.push_back(s1(0, 0) * sex + //The solution for sex with respect to Hospitalization
+		s1(1, 0) * age +				//The solution for age with respect to Hospitalization
+		s1(2, 0) * race +				//The solution for race with respect to Hospitalization
+		s1(3, 0) * medCond);			//The solution for med with respect to Hospitalization
+
+	solution.push_back(s2(0, 0) * sex + //The solution for sex with respect to ICU
+		s2(1, 0) * age +				//The solution for age with respect to ICU
+		s2(2, 0) * race +				//The solution for race with respect to ICU
+		s2(3, 0) * medCond);			//The solution for med with respect to ICU
+		
+	solution.push_back(s3(0, 0) * sex + //The solution for sex with respect to Death
+		s3(1, 0) * age +				//The solution for age with respect to Death
+		s3(2, 0) * race +				//The solution for race with respect to Death
+		s3(3, 0) * medCond);			//The solution for med with respect to Death
+
+	return solution;
+} 
 
 void predict()
 {
@@ -414,7 +492,7 @@ void predict()
 	int inputMed;
 	while (true)
 	{
-		//system("CLS");
+		system("CLS");
 		std::cout << "*********************************************************" << std::endl;
 		std::cout << "Covid Survival Model" << std::endl;
 		std::cout << "*********************************************************" << std::endl;
@@ -502,9 +580,15 @@ void predict()
 	Net myNet(topology);
 	myNet.loadWeights();
 	std::vector<double> send = { outSex, outAge, outRace, outMed };
-	std::vector<double> stats = myNet.predict(send);
+	std::vector<double> statsNN = myNet.predict(send);
 	myNet.viewWeights();
-	std::cout << "Hospitalization chance: " << stats[0] * 100 << "%\tICU chance: " << stats[1] * 100 << "%\tDeath chance: " << stats[2] * 100 << "%" << std::endl;
+
+	linReg();
+	std::vector<double> statsLR = calcLinearReg(outSex, outAge, outRace, outMed);
+
+	std::cout <<"Neural Network Prediction: \t"<<"Hospitalization chance: " << statsNN[0] * 100 << "%\tICU chance: " << statsNN[1] * 100 << "%\tDeath chance: " << statsNN[2] * 100 << "%" << std::endl;
+	std::cout <<"Linear Regression Prediction: \t"<<"Hospitalization chance: " << statsLR[0] * 100 << "%\tICU chance: " << statsLR[1] * 100 << "%\tDeath chance: " << statsLR[2] * 100<<"%"<<std::endl;
+	std::cout<< "Hospitalization Error: " <<error1<< "\tICU Error: " <<error2<< "\tDeath Error: " <<error3<<std::endl;
 
 }
 void nnPicker()
@@ -521,14 +605,39 @@ void nnPicker()
 
 		if (in == 1)
 			train();
-		else
+		else if (in ==2)
 			predict();
+		else break;
     }
     
 }
 
 int main()
 {
+	std::cout << "Please Wait while linear regression computes for dataset." << std::endl;
+	//Calculate matrix stuff first:
+	Eigen::MatrixXd a(642426, 4); //4 columns of input data: Gender, Age, Ethnicity, preExisting?
+	Eigen::MatrixXd b1(642426, 1); //giant column of isHospitalized?
+	Eigen::MatrixXd b2(642426, 1); //giant column of isICU?
+	Eigen::MatrixXd b3(642426, 1); //giant column of isDead?
+
+	loadMatrices(a, b1, b2, b3);
+
+	s1 = a.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b1);
+	error1 = (b1 - a * s1).col(0).squaredNorm() / 642426.0;
+	system("CLS");
+	std::cout << "Please Wait while linear regression computes for dataset.." << std::endl;
+	s2 = a.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b2);
+	error2 = (b2 - a * s2).col(0).squaredNorm() / 642426.0;
+	system("CLS");
+	std::cout << "Please Wait while linear regression computes for dataset..." << std::endl;
+	s3 = a.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b3);
+	error3 = (b3 - a * s3).col(0).squaredNorm() / 642426.0;
+	system("CLS");
+	std::cout << "Done!" << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+
     while(true)
     {
         int input;
@@ -546,5 +655,4 @@ int main()
             break;
         }
     }
-    
 }
